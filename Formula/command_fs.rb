366ea040ca7fb00a800e1f6287c06170e2c2e08e
Alexey Dmitriev <github@admitriev.name>
2020-04-24 03:56:57 +0300

    use git instead of archive

--- Formula/command_fs.rb
+++ Formula/command_fs.rb
@@ -2,12 +2,11 @@ class CommandFs < Formula
   desc "Tool to create FS where each file is processed"
   homepage "https://github.com/AlexeyDmitriev/command_fs"
   version "0.1"
-  stable do
-    url "https://github.com/AlexeyDmitriev/command_fs/archive/v0.1.tar.gz"
-    sha256 "9952dda314aa6692cc3edd13dd5d9537f787ff39849f32ec580f3d93e1539e96"
-  end
 
-  head url "https://github.com/AlexeyDmitriev/command_fs.git"
+  url "https://github.com/AlexeyDmitriev/command_fs.git",
+    :tag=> "v0.1"
+
+  head "https://github.com/AlexeyDmitriev/command_fs.git"
 
   depends_on "cmake" => :build
   depends_on "ninja" => :build
